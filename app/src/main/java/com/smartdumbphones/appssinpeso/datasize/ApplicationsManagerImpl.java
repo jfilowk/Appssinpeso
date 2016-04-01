686c0e1263d5229f2e51c73b8a5c14309e5f4cef
Javier García <javier.tb@outlook.com>
2016-04-01 18:35:49 +0200

    Implementation start

--- app/src/main/java/com/smartdumbphones/appssinpeso/datasize/ApplicationsManagerImpl.java
+++ app/src/main/java/com/smartdumbphones/appssinpeso/datasize/ApplicationsManagerImpl.java
@@ -1,6 +1,11 @@
 package com.smartdumbphones.appssinpeso.datasize;
 
+import android.content.Context;
+import android.content.pm.IPackageStatsObserver;
+import android.content.pm.PackageManager;
 import com.smartdumbphones.appssinpeso.datasize.models.ApplicationInfoStruct;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,9 +16,12 @@ public class ApplicationsManagerImpl implements ApplicationsManager {
 
   private ExecutorService executorService;
   private OnApplicationsListener listener;
+  private Context context;
+  private long packageSize = 0;
 
-  public ApplicationsManagerImpl() {
-    executorService = Executors.newSingleThreadExecutor();
+  public ApplicationsManagerImpl(Context context) {
+    this.context = context;
+    this.executorService = Executors.newSingleThreadExecutor();
   }
 
   @Override public void attachOnApplicationListener(OnApplicationsListener listener) {
@@ -27,14 +35,38 @@ public class ApplicationsManagerImpl implements ApplicationsManager {
         AppDetails appDetails = new AppDetails();
         ArrayList<ApplicationInfoStruct> packages = appDetails.getPackages();
         if (packages.size() == 0) {
-          if (listener != null) {
-            listener.onError();
+          notifyOnError();
+        } else {
+          sortPackagesAlpha(packages);
+
+          for (ApplicationInfoStruct aPackage : packages) {
+            try {
+              PackageManager packageManager = context.getPackageManager();
+              Method getPackageSizeInfo = packageManager.getClass()
+                  .getMethod("getPackageSizeInfo", String.class, IPackageStatsObserver.class);
+              getPackageSizeInfo.invoke(packageManager, aPackage.getPname(),
+                  new CachePackState(aPackage, packageSize));
+              notifyOnSuccess();
+            } catch (SecurityException | NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException e) {
+              e.printStackTrace();
+              notifyOnError();
+            }
           }
         }
-
-        sortPackagesAlpha(packages);
       }
     });
+  }
+
+  private void notifyOnSuccess() {
+    if (listener != null) {
+      listener.onSuccess();
+    }
+  }
+
+  private void notifyOnError() {
+    if (listener != null) {
+      listener.onError();
+    }
   }
 
   private void sortPackagesAlpha(ArrayList<ApplicationInfoStruct> packages) {
