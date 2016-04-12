b3e86731e733c9a4415d17b3bffd963aaae67692
Javier <javier.tb@outlook.com>
2016-04-12 18:13:15 +0200

    Database and CRUD CacheApplication

---
+++ app/src/main/java/com/smartdumbphones/appssinpeso/datasource/cache/CacheApplicationCRUD.java
@@ -0,0 +1,6 @@
+package com.smartdumbphones.appssinpeso.datasource.cache;
+
+public interface CacheApplicationCRUD {
+
+  // TODO: 12/04/2016 Create CRUD methods
+}
---
+++ app/src/main/java/com/smartdumbphones/appssinpeso/datasource/cache/Persistence.java
@@ -0,0 +1,7 @@
+package com.smartdumbphones.appssinpeso.datasource.cache;
+
+import com.smartdumbphones.appssinpeso.models.ApplicationInfoStruct;
+import java.util.List;
+
+public interface Persistence {
+}
