a85fbf904722d5e57be0ba8f906e5dde9891f7a5
Javier <javier.tb@outlook.com>
2016-04-12 17:13:20 +0200

    Class CacheApplicationDB

---
+++ app/src/main/java/com/smartdumbphones/appssinpeso/datasource/cache/CacheApplicationDB.java
@@ -0,0 +1,54 @@
+package com.smartdumbphones.appssinpeso.datasource.cache;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CacheApplicationDB extends SQLiteOpenHelper {
+
+  public static final String APPLICATIONS_TABLE = "applications";
+
+  public static final String ID = "id";
+  public static final String PACKAGE_NAME = "package_name";
+  public static final String APK_SIZE = "apk_size";
+  public static final String CACHE_SIZE = "cache_size";
+  public static final String DATA_SIZE = "data_size";
+  public static final String CREATED_AT = "created_at";
+
+  public static final String CREATE_TABLE_APPLICATIONS = "CREATE TABLE "
+      + APPLICATIONS_TABLE
+      + "("
+      + ID
+      + " integer primery key autoincrement, "
+      + PACKAGE_NAME
+      + " TEXT, "
+      + APK_SIZE
+      + " INTEGER, "
+      + CACHE_SIZE
+      + " INTEGER, "
+      + DATA_SIZE
+      + " INTEGER, "
+      + CREATED_AT
+      + " DATETIME DEFAULT CURRENT_TIMESTAMP";
+
+  public static final String DELETE_TABLE_APPLICATION =
+      "DELETE TABLE IF EXISTS " + APPLICATIONS_TABLE;
+
+  private static final int DB_VERSION = 1;
+  private Context context;
+  private String name;
+
+  public CacheApplicationDB(Context context, String name) {
+    super(context, name, null, DB_VERSION);
+    this.context = context;
+    this.name = name;
+  }
+
+  @Override public void onCreate(SQLiteDatabase db) {
+    db.execSQL(CREATE_TABLE_APPLICATIONS);
+  }
+
+  @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL(DELETE_TABLE_APPLICATION);
+  }
+}
